645b917f95e48b1911923f5c0801d621dcb3165f
MR7 Tecnologia <mr7app@gmail.com>
2020-03-27 09:48:01 -0300

    Week Omnistack 11

---
+++ backend/src/controllers/OngController.js
@@ -0,0 +1,31 @@
+const crypto = require('crypto');
+const connection = require('../database/connection');
+
+module.exports = {
+  //listando todas a ongs cadastradas no bd
+ async index(req,res)  {
+    const ongs = await connection('ongs').select('*');
+  
+    return res.json(ongs);
+  
+  },
+  
+  async create(req,res){
+    console.log(req.body);
+    const { name, email, whatsapp, city, uf } = req.body;
+    const id  = crypto.randomBytes(4).toString('HEX');
+    console.log( name, email, whatsapp, city, uf);
+
+    await  connection('ongs').insert({
+        id,
+        name,
+        email,
+        whatsapp,
+        city,
+        uf,
+      });
+
+    return res.json({id});
+
+  }
+};
